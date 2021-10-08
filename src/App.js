9894851bd11892141748589d992041ae5a63ddbd
MatthewMion <87820540+ShmoneJones@users.noreply.github.com>
2021-10-08 09:51:08 -0400

    finished tasks in addForm

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import { fetchSmurf } from "./actions";
 import AddForm from "./components/AddForm";
 import SmurfList from "./components/SmurfList";
 import Header from "./components/Header";
@@ -7,6 +7,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 class App extends Component {
+  componentDidMount() {
+    fetchSmurf();
+  }
   render() {
     return (
       <div className="App">
@@ -23,6 +26,6 @@ class App extends Component {
 
 export default App;
 
-//Task List:
+//Task List: X
 //1. Connect the fetchSmurfs actions to the App component.
 //2. Call the fetchSmurfs action when the component mounts.
--- src/actions/index.js
+++ src/actions/index.js
@@ -6,10 +6,36 @@ export const FETCH_SMURF_FAIL = "FETCH_SMURF_FAIL";
 export const ADD_SMURF = "ADD_SMURF";
 export const ERROR = "ERROR";
 
-export const fetchSmurf = () => {
-  //   dispatch;
+export const fetchSmurf = () => (dispatch) => {
+  dispatch({ type: FETCH_SMURF_START });
+  axios
+    .get("http://localhost:3333/smurfs")
+    .then((res) => {
+      dispatch({ type: FETCH_SMURF_SUCCESS, payload: res.data });
+    })
+    .catch((err) => {
+      dispatch({ type: FETCH_SMURF_FAIL, payload: err });
+    });
 };
-//Task List:
+
+export const addSmurf = (smurf) => (dispatch) => {
+  dispatch({ type: ADD_SMURF });
+  console.log("addsmurf is working");
+  axios
+    .post("http://localhost:3333/smurfs", smurf)
+
+    .catch((err) => {
+      dispatch({ type: FETCH_SMURF_FAIL, payload: err });
+    });
+};
+
+export const setError = (err) => {
+  return {
+    type: ERROR,
+    payload: err,
+  };
+};
+//Task List: X
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
 //2. Add a standard action that allows us to add new smurf (including the name, nickname, position, summary)
 //3. Add a standard action that allows us to set the value of the error message slice of state.
